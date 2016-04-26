3b31ad22379e512b4b2e37b1baa859ed456910a8
sagalasan <sagalasan@users.noreply.github.com>
2016-04-25 21:17:26 -0600

    Base StringGenerator class

---
+++ src/StringGenerator.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by christiaan on 4/25/16.
+ */
+public class StringGenerator
+{
+  private Random random;
+  private int offset = 'a';
+
+  public StringGenerator()
+  {
+    random = new Random();
+  }
+
+  public List<String> generateStrings(int length, int numStrings)
+  {
+    List<String> strings = new ArrayList<>();
+
+    for(int i = 0; i < numStrings; i++)
+    {
+      strings.add(randomString(length));
+    }
+
+    return strings;
+  }
+
+  public String randomString(int length)
+  {
+    char[] chars = new char[length];
+
+    for(int i = 0; i < length; i++)
+    {
+      char c = (char) (random.nextInt(25) + offset);
+      chars[i] = c;
+    }
+
+    return new String(chars);
+  }
+}
