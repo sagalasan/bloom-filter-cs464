e7217b57eb054d355c18626d0c161d8d30192b7e
sagalasan <sagalasan@users.noreply.github.com>
2016-04-25 20:21:20 -0600

    Base BitArray class

---
+++ src/hash/BitArray.java
@@ -0,0 +1,39 @@
+package hash;
+
+/**
+ * This class simulates a bit array.
+ * We are not allowed to use any libraries so I am simulating a bit array with
+ * a byte array for simplicity.
+ */
+public class BitArray
+{
+  private int arraySize;
+  private byte[] array;
+
+  public BitArray(int arraySize)
+  {
+    this.arraySize = arraySize;
+    array = new byte[arraySize];
+    initArray();
+  }
+
+  private void initArray()
+  {
+    for(int i = 0; i < array.length; i++)
+    {
+      array[i] = 0;
+    }
+  }
+
+  public void setBit(int position)
+  {
+    if(position < 0 || position >= array.length)
+    {
+      throw new IllegalArgumentException("Position is out of bounds");
+    }
+    else
+    {
+      array[position] = 1;
+    }
+  }
+}
