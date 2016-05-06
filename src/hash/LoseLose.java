2e5dddb2e2429d9ed0fe473b9870ba02f1446ebe
sagalasan <sagalasan@users.noreply.github.com>
2016-05-05 20:41:08 -0600

    Implemente LoseLose hash function

--- src/hash/LoseLose.java
+++ src/hash/LoseLose.java
@@ -13,7 +13,14 @@ public class LoseLose extends AbstractHashFunction
   @Override
   public long hash(String string)
   {
-    return 0;
-  }
+    long hash = 0;
+    char[] array = string.toCharArray();
 
+    for(int i = 0; i < array.length; i++)
+    {
+      int c = (int) array[i];
+      hash += c;
+    }
+    return hash;
+  }
 }
